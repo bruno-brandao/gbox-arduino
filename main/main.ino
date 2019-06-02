3518676bd7f4f061418eda51cbc306daf272be89
Bruno Eduardo <brunoed@ucl.br>
2019-06-01 23:06:50 -0300

    correções

--- main/main.ino
+++ main/main.ino
@@ -1,43 +1,74 @@
 #include <EtherCard.h>
 
-// ethernet interface mac address
+static uint32_t timer;
+const char website[] PROGMEM = "192.168.0.17";
+char temp_sensor[150];
+int an0 = A0;
+int sensor = 0;
+
 static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
-// ethernet interface ip address
-static byte myip[] = { 192,168,0,23 };
 
-byte Ethernet::buffer[500];
+byte Ethernet::buffer[700];
 
-BufferFiller bfill;
-
-static homePage(){
-  bfill = ether.tcpOffset();
-  bfill.emit_p(PSTR("Ola mundo!"));
-
-  return bfill.position();
+static void my_callback (byte status, word off, word len)
+{
+  Serial.println(">>>");
+  Ethernet::buffer[off + len] = 0;
+  char* fullResponse;
+  fullResponse = (char*) Ethernet::buffer + off;
+  String str = String(fullResponse);
+  int checkResp = 0;
+  checkResp = str.indexOf(":true");
+  Serial.println(checkResp);
+  if (checkResp != -1) {
+    char json[str.length()];
+    str.toCharArray(json, str.length() + 1);
+    Serial.println(json);
+  }
 }
 
 void setup () {
   Serial.begin(9600);
-  Serial.println("\n[getViaDNS]");
+  Serial.println("Obtendo IP:");
+  Serial.println(F("\n[testDHCP]"));
+
+  Serial.print("MAC: ");
+  for (byte i = 0; i < 6; ++i) {
+    Serial.print(mymac[i], HEX);
+    if (i < 5)
+      Serial.print(':');
+  }
+  Serial.println();
 
   // Change 'SS' to your Slave Select pin, if you arn't using the default pin
-  if (ether.begin(sizeof Ethernet::buffer, mymac, 53) == 0)
-    Serial.println( "Failed to access Ethernet controller");
-  else
-    Serial.println( "Ethernet controller success");
+  if (ether.begin(sizeof Ethernet::buffer, mymac, SS) == 0)
+    Serial.println(F("Failed to access Ethernet controller"));
 
-  ether.staticSetup(myip);
-//
-//  if (!ether.dnsLookup(website))
-//    Serial.println("DNS failed");
-//  ether.printIp("Server: ", ether.hisip);
+  Serial.println(F("Setting up DHCP"));
+  if (!ether.dhcpSetup())
+    Serial.println(F("DHCP failed"));
+
+  ether.printIp("My IP: ", ether.myip);
+  ether.printIp("Netmask: ", ether.netmask);
+  ether.printIp("GW IP: ", ether.gwip);
+  ether.printIp("DNS IP: ", ether.dnsip);
+//  if (!ether.dnsLookup(website))     //IP do servidor por DNS
+//    Serial.println("Falha no DNS");
+  ether.parseIp(ether.hisip, "192.168.0.17"); //IP do servidor manual
+  ether.printIp("Servidor: ", ether.hisip);
+  Serial.println(" ");
 }
 
-void loop () {
-  word pos = ether.packetLoop(ether.packetReceive());
-  Serial.println("success");
-//
-  if (pos) {
-    ether.httpServerReply(homePage());
+void loop ()
+{
+  ether.packetLoop(ether.packetReceive());
+  if (millis() > timer)
+  {
+    timer = millis() + 5000;
+    ether.persistTcpConnection(true);
+    sensor = analogRead(an0);
+    sprintf(temp_sensor, "?val=%d", sensor); 
+    ether.hisport = 8080;//to access  local host
+    ether.browseUrl(PSTR("/gbox/9"), temp_sensor, website, my_callback); 
   }
 }
